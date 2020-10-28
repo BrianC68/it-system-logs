bfc6b2c4c8ac8ccd276cc76175d704530c1ad6c1
Brian Christensen <brianc@wi.rr.com>
2020-10-28 14:59:48 -0500

    first commit through lecture 84

---
+++ src/components/logs/LogItem.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { connect } from 'react-redux'
+import Moment from 'react-moment';
+import PropTypes from 'prop-types';
+import { deleteLog } from '../../actions/logActions';
+import M from 'materialize-css/dist/js/materialize.min.js';
+
+const LogItem = ({ log, deleteLog }) => {
+  const onDelete = () => {
+    deleteLog(log.id);
+    M.toast({ html: 'Log Deleted!' });
+  }
+
+  return (
+    <li className="collection-item">
+      <div>
+        <a href="#edit-log-modal" className={`modal-trigger ${log.attention ? 'red-text' : 'blue-text'}`}>{log.message}</a>
+        <br></br>
+        <span className="grey-text">
+          <span className="black-text">ID #{log.id}</span> Last updated by{' '}
+          <span className="black-text">{log.tech}</span> on{' '}
+          <Moment format='MMMM Do YYYY, h:mm:ss a'>{log.date}</Moment>
+        </span>
+        <a href="#!" className="secondary-content" onClick={onDelete}>
+          <i className="material-icons grey-text">delete</i>
+        </a>
+      </div>
+    </li>
+  )
+}
+
+LogItem.propTypes = {
+  log: PropTypes.object.isRequired,
+  deleteLog: PropTypes.func.isRequired,
+}
+
+export default connect(null, { deleteLog })(LogItem);
